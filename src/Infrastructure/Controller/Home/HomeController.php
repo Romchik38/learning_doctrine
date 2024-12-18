9d2ccc67c7822210ed66efebf52e1e2f05f4b48d
Romchik38 <pomahehko.c@gmail.com>
2024-12-18 17:37:00 +0200

    created logo, home page now with articles

--- src/Infrastructure/Controller/Home/HomeController.php
+++ src/Infrastructure/Controller/Home/HomeController.php
@@ -4,17 +4,28 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller\Home;
 
+use App\Application\ArticleView\ArticleViewService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController{
 
+    public function __construct(
+        protected readonly ArticleViewService $articleViewService
+    )
+    {
+        
+    }
+
     #[Route('/', name: 'homepage_index', methods: ['GET', 'HEAD'])]
     public function index(): Response {
+        $dtos =  $this->articleViewService->listActive();
+
         return $this->render('base.html.twig', [
             'controller_name' => 'HomeController',
-            'controller_template' => 'home/index.html.twig'
+            'controller_template' => 'home/index.html.twig',
+            'active_articles' => $dtos
         ]);
     }
 }
