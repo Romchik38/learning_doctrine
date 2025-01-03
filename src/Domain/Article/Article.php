7ef470a6ca71c0b4ebaccb4ded1bc9e072c41395
Romchik38 <pomahehko.c@gmail.com>
2025-01-03 13:18:14 +0200

    mocking test is created

--- src/Domain/Article/Article.php
+++ src/Domain/Article/Article.php
@@ -37,6 +37,14 @@ class Article
         return $this->id;
     }
 
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
     public function getName(): string
     {
         return $this->name;
---
+++ tests/MockingRepositoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\ArticleView\ArticleViewService;
+use App\Application\ArticleView\Views\ArticleActiveDTO;
+use App\Domain\Article\Article;
+use App\Infrastructure\Controller\Home\HomeController;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MockingRepositoryTest extends KernelTestCase
+{
+    public function testHomePage()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $articleService = $this->createMock(ArticleViewService::class);
+        $article1 = new ArticleActiveDTO(
+            1,
+            'Some article',
+            'Some short description'
+        );
+
+        $articleService->expects($this->once())->method('listActive')
+            ->willReturn([
+                $article1,
+            ]);
+        
+        $container->set(ArticleViewService::class, $articleService);
+        $homeController = $container->get(HomeController::class);
+        $homeController->index();
+    }
+}
