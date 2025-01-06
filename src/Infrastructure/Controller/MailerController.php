e46274cf71564943a9a9b855b694222bef3c706c
Romchik38 <pomahehko.c@gmail.com>
2025-01-06 15:54:28 +0200

    created a mail controller

---
+++ src/Infrastructure/Controller/MailerController.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+// src/Controller/MailerController.php
+namespace App\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('ser@mail.ozone.com.ua')
+            ->to('ser@mail.ozone.com.ua')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return new Response('message sent');
+    }
+}
