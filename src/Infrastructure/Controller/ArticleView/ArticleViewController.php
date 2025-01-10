d37773af5d764a01248323e7457bb6300168abfd
Romchik38 <pomahehko.c@gmail.com>
2025-01-10 21:01:05 +0200

    last viewed article is ready

--- src/Infrastructure/Controller/ArticleView/ArticleViewController.php
+++ src/Infrastructure/Controller/ArticleView/ArticleViewController.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller\ArticleView;
 
 use App\Application\ArticleView\ArticleViewService;
+use App\Application\LastVisitedArticles\CouldNotSaveArticleException;
+use App\Application\LastVisitedArticles\LastVisitedArticlesService;
+use App\Application\LastVisitedArticles\Views\Article;
 use App\Domain\Article\NoSuchArticleException;
 use InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,6 +20,8 @@ final class ArticleViewController extends AbstractController
 
     public function __construct(
         protected readonly ArticleViewService $articleViewService,
+        protected readonly LastVisitedArticlesService $lastVisitedArticlesService,
+        protected readonly LoggerInterface $logger
     ) {}
 
     #[Route('/article/{id}', name: 'article_view', methods: ['GET', 'HEAD'])]
@@ -23,6 +29,23 @@ final class ArticleViewController extends AbstractController
     {
         try {
             $articleDto = $this->articleViewService->findActive($id);
+
+            try {
+                $this->lastVisitedArticlesService->saveLastArticle(
+                    new Article(
+                        $articleDto->name,
+                        $this->generateUrl('article_view', ['id' => $id])
+                    )
+                );
+            } catch (CouldNotSaveArticleException) {
+                $this->logger->error(
+                    sprintf(
+                        'Last viewed article service did not save article with id %s',
+                        $id
+                    )
+                );
+            }
+
             return $this->render('base.html.twig', [
                 'controller_name' => 'ArticleController',
                 'controller_template' => 'home/article/view.html.twig',
