1c8d7344700d3fc1248fe2e81010be13f5626bd6
Romchik38 <pomahehko.c@gmail.com>
2025-01-22 11:50:44 +0200

    added restrictions

--- src/Infrastructure/Controller/Admin/Article/ArticleViewController.php
+++ src/Infrastructure/Controller/Admin/Article/ArticleViewController.php
@@ -10,7 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\UrlHelper;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 final class ArticleViewController extends AbstractController
 {
 
