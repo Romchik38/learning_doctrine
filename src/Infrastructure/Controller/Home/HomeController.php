834be050d9d5ca3e2f35dc00a38e6a7637bca7fd
Romchik38 <pomahehko.c@gmail.com>
2025-01-22 14:18:07 +0200

    refactored

--- src/Infrastructure/Controller/Home/HomeController.php
+++ src/Infrastructure/Controller/Home/HomeController.php
@@ -29,12 +29,9 @@ final class HomeController extends AbstractController
         private LocaleSwitcher $localeSwitcher
     ) {}
 
-    #[Route( path: '/', name: 'homepage_index', methods: ['GET', 'HEAD'])]
+    #[Route(path: '/', name: 'homepage_index', methods: ['GET', 'HEAD'])]
     public function index(Request $request): Response
     {
-        // $locale = $request->getLocale();
-        // $prefered = $request->getPreferredLanguage(['uk', 'en'] );
-        // $this->localeSwitcher->setLocale($prefered);
 
         $dtos =  $this->articleViewService->listActive();
 
