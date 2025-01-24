917d176edce7a1e1a3409db5ea10bde753bfb33d
Romchik38 <pomahehko.c@gmail.com>
2025-01-24 17:31:19 +0200

    assets updated

--- importmap.php
+++ importmap.php
@@ -16,16 +16,16 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
     'home' => [
-        'path' => './assets/js/home.js'
-    ]
+        'path' => './assets/js/home.js',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '8.0.12',
+    ],
 ];
