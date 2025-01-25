e183214fa41fdaec92ed8fe6cf1ddf201ff89f16
Romchik38 <pomahehko.c@gmail.com>
2025-01-25 14:36:33 +0200

    created first hello app

--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static targets = [ "name" ]
+
     connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+        // this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    }
+
+    greet() {
+        const element = this.nameTarget
+        const name = element.value
+        console.log(`Hello, ${name}!`)
     }
 }
